7e899bba4714f8689ea60c97dae86b123bd91cc7
Alex Dunn <alexander.dunn27@gmail.com>
2022-04-20 20:28:57 -0700

    distinguish between inward and outward normals

--- RTX/sphere.h
+++ RTX/sphere.h
@@ -9,7 +9,6 @@ public:
 	double radius;
 
 public:
-	sphere() {}
 	sphere(point3 cen, double r) : center(cen), radius(r) {};
 
 	virtual bool hit(const ray& r, double t_min, double t_max, hit_record& rec) const override {
@@ -33,7 +32,8 @@ public:
 
 		rec.t = root;
 		rec.p = r.at(rec.t);
-		rec.normal = (rec.p - center) / radius;
+		vec3 outward_normal = (rec.p - center) / radius;
+		rec.set_face_normal(r, outward_normal);
 
 		return true;
 	}
--- RTX/vec3.h
+++ RTX/vec3.h
@@ -53,6 +53,10 @@ inline vec3 operator-(const vec3& v1, const vec3& v2) {
 	return vec3(v1[0] - v2[0], v1[1] - v2[1], v1[2] - v2[2]);
 }
 
+inline vec3 operator-(const vec3& v) {
+	return vec3(-v[0], -v[1], -v[2]);
+}
+
 inline double vector_dot(const vec3& v1, const vec3& v2) {
 	return v1[0] * v2[0]
 		+ v1[1] * v2[1]
