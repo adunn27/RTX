352cb985e0f6982306b0310853a470fe1ff9a172
Alex Dunn <alexander.dunn27@gmail.com>
2022-04-22 02:29:05 -0700

    added glass like material

--- RTX/material.h
+++ RTX/material.h
@@ -45,3 +45,37 @@ public:
 	}
 };
 
+class dielectric : public material {
+public:
+	double refraction_idx;
+
+private:
+	static double reflectance(double cos, double refr_idx) {
+		// Schlick's apprx
+		auto r0 = pow((1 - refr_idx) / (1 + refr_idx), 2);
+		return r0 + (1 - r0) * pow(1 - cos, 5);
+	}
+
+public:
+	dielectric(double refraction_idx) : refraction_idx(refraction_idx) {}
+
+	virtual bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override {
+		attenuation = color(1.0, 1.0, 1.0);
+		double refr_ratio = rec.front_face ? (1.0 / refraction_idx) : refraction_idx;
+
+		vec3 unit_dir = r_in.direction().unit_vector();
+
+		double cos_theta = fmin(vector_dot(-unit_dir, rec.normal), 1.0);
+		double sin_theta = sqrt(1.0 - cos_theta * cos_theta);
+
+		vec3 dir;
+		if (refr_ratio * sin_theta > 1.0 || reflectance(cos_theta, refr_ratio) > random_double())
+			dir = reflect(unit_dir, rec.normal);
+		else
+			dir = refract(unit_dir, rec.normal, refr_ratio);
+
+		scattered = ray(rec.p, dir);
+		return true;
+	}
+};
+
