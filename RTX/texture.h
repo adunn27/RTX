e5de620116fb0d70e38730a81001723be0527925
Alex Dunn <alexander.dunn27@gmail.com>
2022-04-23 02:37:39 -0700

    add checker texture

--- RTX/texture.h
+++ RTX/texture.h
@@ -18,3 +18,23 @@ public:
 		return color_val;
 	}
 };
+
+class checker_texture : public texture {
+public:
+	std::shared_ptr<texture> odd;
+	std::shared_ptr<texture> even;
+
+public:
+	checker_texture(color c1, color c2) :
+		odd(std::make_shared<solid_color>(c1)), even(std::make_shared<solid_color>(c2)) {}
+
+	virtual color value(double u, double v, const point3& p) const override {
+		auto sine = sin(10 * p.x()) * sin(10 * p.y()) * sin(10 * p.z());
+		if (sine < 0)
+			return odd->value(u, v, p);
+		else
+			return even->value(u, v, p);
+	}
+
+};
+
