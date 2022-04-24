0e7f490baf05b2fbdb15ba9e74b7e1d93c8274d0
Alex Dunn <alexander.dunn27@gmail.com>
2022-04-23 17:43:33 -0700

    add image textures

--- RTX/texture.h
+++ RTX/texture.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "vec3.h"
+#include "rtw_stbh_image.h"
 
 class texture {
 public:
@@ -16,6 +17,53 @@ public:
 
 	virtual color value(double u, double v, const vec3& p) const override {
 		return color_val;
+	}
+};
+
+class image_texture : public texture {
+public:
+	static const int bytes_per_pixel = 3;
+
+private:
+	unsigned char* data; // The loaded file
+	int width;
+	int height;
+	int bytes_per_scanline;
+
+
+public:
+	image_texture(const char* filename) {
+		auto components_per_pixel = bytes_per_pixel;
+		data = stbi_load(filename, &width, &height, &components_per_pixel, components_per_pixel);
+
+		if (!data) {
+			std::cerr << "ERROR: Could not load texture file '" << filename << "'\n";
+			width = 0;
+			height = 0;
+		}
+
+		bytes_per_scanline = bytes_per_pixel * width;
+	}
+
+	~image_texture() {
+		delete data;
+	}
+
+	virtual color value(double u, double v, const point3& p) const override {
+		if (data == nullptr)
+			return color(0, 1, 1); // Cyan as default color in case of error
+
+		// Convert to image coordinates
+		u = clamp(u, 0.0, 1.0);
+		v = 1.0 - clamp(v, 0.0, 1.0); // Need to flip v
+
+		auto i = static_cast<int>(u * width);
+		auto j = static_cast<int>(v * height);
+
+		const auto color_scale = 1.0 / 255.0;
+		auto pixel = data + j * bytes_per_scanline + i * bytes_per_pixel;
+
+		return color(color_scale * pixel[0], color_scale * pixel[1], color_scale * pixel[2]);
 	}
 };
 
