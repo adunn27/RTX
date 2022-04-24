bf5f07ee18e84b4d8c66e85484848ab3eebf9277
Alex Dunn <alexander.dunn27@gmail.com>
2022-04-24 02:45:17 -0700

    move box compares before class

--- RTX/bvh.h
+++ RTX/bvh.h
@@ -7,6 +7,32 @@
 #include "utility.h"
 #include <iostream>
 
+inline bool box_compare(
+	const std::shared_ptr<hittable> a,
+	const std::shared_ptr<hittable> b,
+	int axis
+) {
+	aabb box_a;
+	aabb box_b;
+
+	if (!a->bounding_box(0, 0, box_a) || !b->bounding_box(0, 0, box_b))
+		std::cerr << "No bounding box in bvh_node constructor.\n";
+
+	return box_a.min[axis] < box_b.min[axis];
+}
+
+bool box_x_compare(const std::shared_ptr<hittable> a, const std::shared_ptr<hittable> b) {
+	return box_compare(a, b, 0);
+}
+
+bool box_y_compare(const std::shared_ptr<hittable> a, const std::shared_ptr<hittable> b) {
+	return box_compare(a, b, 1);
+}
+
+bool box_z_compare(const std::shared_ptr<hittable> a, const std::shared_ptr<hittable> b) {
+	return box_compare(a, b, 2);
+}
+
 class bvh_node : public hittable {
 public:
 	std::shared_ptr<hittable> left;
@@ -69,30 +95,4 @@ public:
 		return true;
 	}
 };
-
-inline bool box_compare(
-	const std::shared_ptr<hittable> a,
-	const std::shared_ptr<hittable> b,
-	int axis
-) {
-	aabb box_a;
-	aabb box_b;
-
-	if (!a->bounding_box(0, 0, box_a) || !b->bounding_box(0, 0, box_b))
-		std::cerr << "No bounding box in bvh_node constructor.\n";
-
-	return box_a.min[axis] < box_b.min[axis];
-}
-
-bool box_x_compare(const std::shared_ptr<hittable> a, const std::shared_ptr<hittable> b) {
-	return box_compare(a, b, 0);
-}
-
-bool box_y_compare(const std::shared_ptr<hittable> a, const std::shared_ptr<hittable> b) {
-	return box_compare(a, b, 1);
-}
-
-bool box_z_compare(const std::shared_ptr<hittable> a, const std::shared_ptr<hittable> b) {
-	return box_compare(a, b, 2);
-}
 
